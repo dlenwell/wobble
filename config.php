445aacd5cdfbeffc3802b4338ff9229c34771270
David Lenwell <dlenwell@gmail.com>
2013-04-25 22:08:37 -0700

    modified config and added license to readme

--- config.php
+++ config.php
@@ -1,22 +1,26 @@
 <?php
 	class config
 	{
-		public $MASTER_PAGE				= "master.html";
-		public $DEFAULT_TEMPLATE_PATH 	= '/home/web/vpsrobot.com/library/templates/';
-		public $SITE_NAME 				= 'VPS Robot';
-		public $LIB_PATH 				= '/home/web/vpsrobot.com/library/';
-		public $PUB_PATH				= '/home/web/vpsrobot.com/public';
-		public $MODULES 				= array('content','admin', 'fourofour', 'questions','blog');
-		public $DEFAULT_MODULE 			= 'content';
-		public $MODULES_ADMIN			= array("admin");
-		//public $redirects				= array("admin" => "/module/public/help");
-		public $MYSQL_USER 				= "web_user";
-		public $MYSQL_PASSWORD			= "4AZtq5yJ";
-		public $MYSQL_HOST 				= "mysql01.vpsrobot.com";
-		public $MYSQLSCHEMA 			= "vpsrobot";
-		public $MESSAGE_NOT_LOGGED_IN	= "Please Log in and we'll speed you on your way!";
+	    public $BASE_DIR        = $current_file_path = dirname(__FILE__);
+		public $THEME		    = "blue_robot";
+		
+		
+		public $SITE_NAME 		= 'Wobble';
+		public $LIB_PATH 		= $BASE_DIR.'/library/';
+		public $PUB_PATH		= $BASE_DIR.'/public';
+		public $MODULES 		= array('content','admin', 'fourofour', 'questions','blog');
+		public $DEFAULT_MODULE 	= 'content';
+		public $MODULES_ADMIN	= array("admin");
+		public $redirects		= array("admin" => "/module/public/help");
+		
+		
+		public $DB_USER 		= "wobble_user";
+		public $DB_PASSWORD		= "4AZtq5yJ";
+		public $DB_HOST 		= "localhost";
+		public $DB_SCHEMA 		= "wobble";
+		
 	}
 
 
-//'blog',	
+
 ?>
